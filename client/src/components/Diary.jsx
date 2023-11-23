b334594e16883631d15382c9d7a470def5ab4b70
joshkat <joshua02k@gmail.com>
2023-11-23 12:38:55 -0500

    Added backendless date selector

--- client/src/components/Diary.jsx
+++ client/src/components/Diary.jsx
@@ -5,16 +5,18 @@ import PropTypes from "prop-types";
 function Diary() {
     // pull user food info from db of this shape
     const [userFoodArr, setUserFoodArr] = useState([
-        [{ name: "Eggo Waffles", id: "1", calories: 90, carb:0, fat:0, protein:0, servings:1 }], // breakfast
-        [{ name: "Kirkland Protein Bar", id: "2", calories: 180, carb:0, fat:0, protein:0, servings:1 }, { name: "Eggo Waffles", id: "1", calories: 90, carb:0, fat:0, protein:0, servings:1}], // lunch
-        [{ name: "Kirkland Protein Bar", id: "2", calories: 180, carb:0, fat:0, protein:0, servings:1 }], // dinner
-        [{ name: "Kirkland Protein Bar", id: "2", calories: 180, carb:0, fat:0, protein:0, servings:1 }], // snacks
+        [{ name: "Eggo Waffles", id: "1", calories: 90, carb: 0, fat: 0, protein: 0, servings: 1 }], // breakfast
+        [{ name: "Kirkland Protein Bar", id: "2", calories: 180, carb: 0, fat: 0, protein: 0, servings: 1 }, { name: "Eggo Waffles", id: "1", calories: 90, carb: 0, fat: 0, protein: 0, servings: 1 }], // lunch
+        [{ name: "Kirkland Protein Bar", id: "2", calories: 180, carb: 0, fat: 0, protein: 0, servings: 1 }], // dinner
+        [{ name: "Kirkland Protein Bar", id: "2", calories: 180, carb: 0, fat: 0, protein: 0, servings: 1 }], // snacks
     ]);
 
     const titles = ["Breakfast", "Lunch", "Dinner", "Snacks"];
 
     return (
         <div>
+            <div className="divider m-0"></div>
+            <DateSelector setUserFoodArr={setUserFoodArr}/>
             <div className="divider m-0"></div>
             <CalorieRemain />
             <div className="divider m-0"></div>
@@ -25,6 +27,61 @@ function Diary() {
                 </div>
             ))}
         </div>
+    )
+}
+
+function DateSelector({ setUserFoodArr }) {
+    const d = new Date();
+    const dateMap = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const monthMap = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const [date, setDate] = useState(getCurrentDate());
+    const [writtenDate, setWrittenDate] = useState(`${dateMap[d.getDay()]}, ${monthMap[d.getMonth()]} ${d.getDate()}`); // takes form of `Sunday, Junary 01`
+
+    function updateDate(e){
+        //function will need to change foodArr as well
+        e.preventDefault();
+        const inputDate = e.target.form[0].value; // "yyyy-mm-dd"
+        const [year, month, day] = inputDate.split("-").map(Number);
+        const d = new Date(year, month - 1, day);
+        
+        setDate(e.target.form[0].value);
+        setWrittenDate(`${dateMap[d.getDay()]}, ${monthMap[d.getMonth()]} ${d.getDate()}`);
+
+        // at this point pull from db using updated date state
+        setUserFoodArr([[],[],[],[]]); //for now update to empty arr
+    }
+
+    function getCurrentDate() {
+        const date = new Date();
+        const month = date.getMonth() + 1; // getMonth() returns 0-11
+        const day = date.getDate(); // getDate() returns 1-31
+        const year = date.getFullYear(); // getFullYear() returns the year
+    
+        // Pad single digit month and day with a leading zero
+        const formattedMonth = month < 10 ? `0${month}` : month;
+        const formattedDay = day < 10 ? `0${day}` : day;
+    
+        return `${year}-${formattedMonth}-${formattedDay}`;
+    }
+
+    return (
+        <>
+            <button className="btn btn-block btn-accent" onClick={() => document.getElementById('date_modal').showModal()}>{writtenDate}</button>
+            <dialog id="date_modal" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg text-center">Enter the new date below</h3>
+                        <form action="" className="flex justify-evenly">
+                            <input type="date" />
+                            <button className="btn" onClick={(e) => {updateDate(e)}}>Go there</button>
+                        </form>
+                </div>
+            </dialog>
+        </>
     )
 }
 
@@ -61,26 +118,26 @@ function MealTable({ title, foodArr, userFoodArr, setUserFoodArr }) {
         <ul className="menu bg-base-200 p-0 [&_li>*]:rounded-none">
             <li className="menu-title">{title}</li>
             {foodArr.map((foodObj, index) => (
-                <FoodTableItem 
-                    name={foodObj.name} 
-                    id={foodObj.id} 
-                    cal={foodObj.calories} 
-                    carb={foodObj.calories === 0 ? 0 : Math.floor((foodObj.carb*4 / (foodObj.calories/foodObj.servings)) * 100)} 
-                    fat={foodObj.calories === 0 ? 0 : Math.floor((foodObj.fat*9 / (foodObj.calories/foodObj.servings)) * 100)} 
-                    protein={foodObj.calories === 0 ? 0 : Math.floor((foodObj.protein*4 / (foodObj.calories/foodObj.servings)) * 100)}
-                    servings={foodObj.servings} 
-                    key={index} 
+                <FoodTableItem
+                    name={foodObj.name}
+                    id={foodObj.id}
+                    cal={foodObj.calories}
+                    carb={foodObj.calories === 0 ? 0 : Math.floor((foodObj.carb * 4 / (foodObj.calories / foodObj.servings)) * 100)}
+                    fat={foodObj.calories === 0 ? 0 : Math.floor((foodObj.fat * 9 / (foodObj.calories / foodObj.servings)) * 100)}
+                    protein={foodObj.calories === 0 ? 0 : Math.floor((foodObj.protein * 4 / (foodObj.calories / foodObj.servings)) * 100)}
+                    servings={foodObj.servings}
+                    key={index}
                 />
             ))}
             <div className="divider m-0"></div>
-            <button onClick={() => { document.getElementById(`my_modal_${title}`).showModal(); }} className="btn btn-neutral btn-outline" id={title}>ADD FOOD</button>
-            <AddCustomFoodModal title={title} userFoodArr={userFoodArr} setUserFoodArr={setUserFoodArr}/>
+            <button onClick={() => { document.getElementById(`my_modal_${title}`).showModal(); }} className="btn btn-outline btn-primary" id={title}>ADD FOOD</button>
+            <AddCustomFoodModal title={title} userFoodArr={userFoodArr} setUserFoodArr={setUserFoodArr} />
 
         </ul>
     )
 }
 
-function FoodTableItem({ name, id, cal, carb, protein, fat, servings}) {
+function FoodTableItem({ name, id, cal, carb, protein, fat, servings }) {
     return (
         <>
             <div className="divider m-0"></div>
@@ -113,18 +170,18 @@ function FoodTableItem({ name, id, cal, carb, protein, fat, servings}) {
     )
 }
 
-function AddCustomFoodModal({title, userFoodArr, setUserFoodArr}){
-    const [foodInfo, updateFoodInfo] = useState({name: "", carb:0, fat:0, protein:0, servings:1});
+function AddCustomFoodModal({ title, userFoodArr, setUserFoodArr }) {
+    const [foodInfo, updateFoodInfo] = useState({ name: "", carb: 0, fat: 0, protein: 0, servings: 1 });
     const [cal, updateCal] = useState(0);
 
-    function updateFood(value, macro, inputBox){
+    function updateFood(value, macro, inputBox) {
         let temp = JSON.parse(JSON.stringify(foodInfo));
 
-        if(value === "" || parseFloat(value) < 0 || (macro === "servings" && parseFloat(value) <= 0)){
+        if (value === "" || parseFloat(value) < 0 || (macro === "servings" && parseFloat(value) <= 0)) {
             console.error("Bad input value");
             inputBox.classList.add("input-error");
             temp[macro] = 0; //reset val for that macro to 0 when bad input
-            if(macro === "name"){ //reset to "" for food name
+            if (macro === "name") { //reset to "" for food name
                 temp[macro] = "";
             }
         } else {
@@ -133,10 +190,10 @@ function AddCustomFoodModal({title, userFoodArr, setUserFoodArr}){
             isNaN(parseFloat(value)) ? temp[macro] = value : temp[macro] = parseFloat(value);
         }
         updateFoodInfo(temp);
-        updateCal(Math.floor(temp.carb*4 + temp.fat*9 + temp.protein*4));
+        updateCal(Math.floor(temp.carb * 4 + temp.fat * 9 + temp.protein * 4));
     }
 
-    function addFood(e, foodInfo, mealName){
+    function addFood(e, foodInfo, mealName) {
         const mealsMap = {
             "Breakfast": 0,
             "Lunch": 1,
@@ -144,83 +201,83 @@ function AddCustomFoodModal({title, userFoodArr, setUserFoodArr}){
             "Snacks": 3
         };
         const mealIndex = mealsMap[mealName];
-        e.preventDefault(); 
+        e.preventDefault();
         console.log(foodInfo);
-        if(foodInfo.servings <= 0){
+        if (foodInfo.servings <= 0) {
             alert("Enter a serving amount >= 1");
             return;
-        } else if(foodInfo.name === ""){
+        } else if (foodInfo.name === "") {
             alert("Enter a food name of length >= 1");
             return;
         }
-    
+
         const newUserFoodArr = [...userFoodArr];
-        const newFood = {...foodInfo, calories: cal*foodInfo.servings, id: `${Math.floor(Math.random() * 9999)}`};
+        const newFood = { ...foodInfo, calories: cal * foodInfo.servings, id: `${Math.floor(Math.random() * 9999)}` };
         if (!newUserFoodArr[mealIndex]) {
             newUserFoodArr[mealIndex] = []; // Initialize the array if it doesn't exist
         }
         newUserFoodArr[mealIndex].push(newFood);
-    
+
         console.log(newUserFoodArr[mealIndex]);
         setUserFoodArr(newUserFoodArr); // Update the state with the new array
     }
-    
+
     return (
         <dialog id={`my_modal_${title}`} className="modal">
-        <div className="modal-box h-screen">
-            <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-            </form>
+            <div className="modal-box h-screen">
+                <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
 
-            <h3 className="font-bold text-lg">Add {title} Food</h3>
-            <form action="">
+                <h3 className="font-bold text-lg">Add {title} Food</h3>
+                <form action="">
 
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Food Name</span>
-                    </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "name", e.target)}/>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Food Name</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "name", e.target)} />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Carbohydrates (grams)</span>
+                        </label>
+                        <input type="number" placeholder="0" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "carb", e.target)} />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Fat (grams)</span>
+                        </label>
+                        <input type="number" placeholder="0" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "fat", e.target)} />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Protein (grams)</span>
+                        </label>
+                        <input type="number" placeholder="0" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "protein", e.target)} />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Servings</span>
+                        </label>
+                        <input type="number" placeholder="1" defaultValue={1} className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "servings", e.target)} />
+                    </div>
+
+                    <button className="btn" onClick={(e) => { addFood(e, foodInfo, title) }}>Add to {title}</button>
+                </form>
+                <div className="grid grid-cols-2 place-items-center sm:flex sm:justify-evenly">
+                    <div className="radial-progress text-xs text-center text-rose-500" style={{ "--value": cal === 0 ? 0 : Math.floor((foodInfo.carb * 4 / cal) * 100) }} role="progressbar">Carbs <br /> {cal === 0 ? 0 : Math.floor((foodInfo.carb * 4 / cal) * 100)} %</div>
+                    <div className="radial-progress text-xs text-center text-teal-500" style={{ "--value": cal === 0 ? 0 : Math.floor((foodInfo.fat * 9 / cal) * 100) }} role="progressbar">Fat <br /> {cal === 0 ? 0 : Math.floor((foodInfo.fat * 9 / cal) * 100)} %</div>
+                    <div className="radial-progress text-xs text-center text-cyan-500" style={{ "--value": cal === 0 ? 0 : Math.floor((foodInfo.protein * 4 / cal) * 100) }} role="progressbar">Protein <br /> {cal === 0 ? 0 : Math.floor((foodInfo.protein * 4 / cal) * 100)} %</div>
+
+                    <div className="radial-progress text-xs text-center " style={{ "--value": 100 }} role="progressbar">Calories <br /> {foodInfo.servings > 0 ? cal * foodInfo.servings : cal} </div>
                 </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Carbohydrates (grams)</span>
-                    </label>
-                    <input type="number" placeholder="0" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "carb", e.target)}/>
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Fat (grams)</span>
-                    </label>
-                    <input type="number" placeholder="0" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "fat", e.target)} />
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Protein (grams)</span>
-                    </label>
-                    <input type="number" placeholder="0" className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "protein", e.target)}/>
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Servings</span>
-                    </label>
-                    <input type="number" placeholder="1" defaultValue={1} className="input input-bordered w-full max-w-xs" onInput={(e) => updateFood(e.target.value, "servings", e.target)}/>
-                </div>
-
-                <button className="btn" onClick={(e) => {addFood(e, foodInfo, title)}}>Add to {title}</button>
-            </form>
-            <div className="grid grid-cols-2 place-items-center sm:flex sm:justify-evenly">
-                <div className="radial-progress text-xs text-center text-rose-500" style={{ "--value": cal === 0 ? 0 : Math.floor((foodInfo.carb*4 / cal) * 100) }} role="progressbar">Carbs <br /> {cal === 0 ? 0 : Math.floor((foodInfo.carb*4 / cal) * 100)} %</div>
-                <div className="radial-progress text-xs text-center text-teal-500" style={{ "--value": cal === 0 ? 0 : Math.floor((foodInfo.fat*9 / cal) * 100) }} role="progressbar">Fat <br /> {cal === 0 ? 0 : Math.floor((foodInfo.fat*9 / cal) * 100)} %</div> 
-                <div className="radial-progress text-xs text-center text-cyan-500" style={{ "--value": cal === 0 ? 0 : Math.floor((foodInfo.protein*4 / cal) * 100) }} role="progressbar">Protein <br /> {cal === 0 ? 0 : Math.floor((foodInfo.protein*4 / cal) * 100)} %</div>
-
-                <div className="radial-progress text-xs text-center " style={{ "--value": 100 }} role="progressbar">Calories <br /> {foodInfo.servings > 0 ? cal * foodInfo.servings : cal} </div> 
             </div>
-        </div>
         </dialog>
     )
 }
